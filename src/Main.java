78710b72750bb3dba5607fad7acea655fd9772a9
Jeremy <jeremy.jakob11@gmail.com>
2024-01-15 17:06:39 +0100

    connection interface

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@
 // then press Enter. You can now see whitespace characters in your code.
 import java.util.List;
 import java.util.ArrayList;
+
+
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
@@ -130,6 +132,98 @@ public class Main {
         }
 
         class Event {
+        }
+
+// Connection interface
+        abstract class Connection {
+            public abstract void transmitData(String data);
+            public abstract String receiveData();
+        }
+
+// Implementation of Connection interface
+        class BasicConnection extends Connection {
+            private List<NetworkInterface> interfaces;
+
+            // Constructor, getters, setters, etc.
+
+            @Override
+            public void transmitData(String data) {
+                // Implementation for data transmission
+            }
+
+            @Override
+            public String receiveData() {
+                // Implementation for receiving data
+                return null;
+            }
+
+            // Additional methods if needed
+        }
+
+// TrafficDelayStrategy interface
+        interface TrafficDelayStrategy {
+            void applyDelay();
+        }
+
+// Implementation of TrafficDelayStrategy
+        class BasicTrafficDelayStrategy implements TrafficDelayStrategy {
+            @Override
+            public void applyDelay() {
+                // Implementation for applying traffic delay
+            }
+        }
+
+// EnvironmentalEffectStrategy interface
+        interface EnvironmentalEffectStrategy {
+            void applyEffect();
+        }
+
+// Implementation of EnvironmentalEffectStrategy
+        class BasicEnvironmentalEffectStrategy implements EnvironmentalEffectStrategy {
+            @Override
+            public void applyEffect() {
+                // Implementation for applying environmental effect
+            }
+        }
+
+// Wifi class implementing Connection interface
+        class Wifi extends Connection {
+            private String ssid;
+
+            // Constructor, getters, setters, etc.
+
+            @Override
+            public void transmitData(String data) {
+                // Implementation specific to Wifi
+            }
+
+            @Override
+            public String receiveData() {
+                // Implementation specific to Wifi
+                return null;
+            }
+
+            // Additional methods if needed
+        }
+
+// Ethernet class implementing Connection interface
+        class Ethernet extends Connection {
+            private int portNumber;
+
+            // Constructor, getters, setters, etc.
+
+            @Override
+            public void transmitData(String data) {
+                // Implementation specific to Ethernet
+            }
+
+            @Override
+            public String receiveData() {
+                // Implementation specific to Ethernet
+                return null;
+            }
+
+            // Additional methods if needed
         }
 
     }
